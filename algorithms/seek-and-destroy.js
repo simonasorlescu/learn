72695885bfe4ad61560eebd95c2b2d9eba2bff09
simonasorlescu <simona.sorlescu@gmail.com>
2016-04-20 23:38:49 +0100

    seek and destroy: change to es6

--- algorithms/seek-and-destroy.js
+++ algorithms/seek-and-destroy.js
@@ -1,15 +1,8 @@
 // Remove all elements from an array that are of the same value as the arguments provided.
 function destroyer(arr) {
-	var args = Array.from(arguments).slice(1),
-		output = [];
 
-	for (var i = 0, len = arr.length; i < len; i++) {
-		// Check if element in array is equal to argument
-		if (args.indexOf(arr[i]) === -1) {
-			output.push(arr[i]);
-		}
-	}
-	return output;
+  var args = new Set(Array.from(arguments).slice(1));
+  return arr.filter(val => !args.has(val));
 }
 
 destroyer([3, 5, 1, 2, 2], 2, 3, 5);
